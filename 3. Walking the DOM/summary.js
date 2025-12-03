b71999a33a9a5a1c8a25f892b960433101d84bd2
vetacode <vetacode@gmail.com>
2025-12-03 07:10:27 +0700

    adding remarks on topic 3 walking the dom

---
+++ 3. Walking the DOM/summary.js
@@ -0,0 +1,21 @@
+/**
+ * The topmost tree nodes are available directly as document properties:
+
+<html> = document.documentElement
+The topmost document node is document.documentElement. That’s the DOM node of the <html> tag.
+
+<body> = document.body
+Another widely used DOM node is the <body> element – document.body.
+
+<head> = document.head
+The <head> tag is available as document.head.
+ * 
+ * 
+ * 
+ * Given a DOM node, we can go to its immediate neighbors using navigation properties.
+
+There are two main sets of them:
+
+For all nodes: parentNode, childNodes, firstChild, lastChild, previousSibling, nextSibling.
+For element nodes only: parentElement, children, firstElementChild, lastElementChild, previousElementSibling, nextElementSibling.
+ */
