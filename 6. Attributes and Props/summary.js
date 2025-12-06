1f6090b029221e09fd033ebea195478701399806
vetacode <vetacode@gmail.com>
2025-12-06 13:42:56 +0700

    adds remarks on all htmls files and adds summary

---
+++ 6. Attributes and Props/summary.js
@@ -0,0 +1,22 @@
+/**
+Attributes – is what’s written in HTML.
+Properties – is what’s in DOM objects.
+
+
+      | Properties	                                                        | Attributes
+-----------------------------------------------------------------------------------------------
+Type	| Any value, standard properties have types described in the spec	    | A string
+Name	| Name is case-sensitive                              	              | Name is not case-sensitive
+
+Methods to work with attributes are:
+
+elem.hasAttribute(name) – to check for existence.
+elem.getAttribute(name) – to get the value.
+elem.setAttribute(name, value) – to set the value.
+elem.removeAttribute(name) – to remove the attribute.
+elem.attributes is a collection of all attributes.
+
+
+ * 
+ * 
+ */
